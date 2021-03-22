69d4f48139f31020fe5a2dc2a0679d1a789938a7
Mahesh Sabnis <sabnis_m@hotmail.com>
2021-03-22 16:44:31 +0530

    Session State

---
+++ Blazor_SessionManagement/Models/ModelClasses.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_SessionManagement.Models
+{
+	public class Person
+	{
+		public string PersonName { get; set; }
+	}
+}
