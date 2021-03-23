711b05b56e89ef4e0866168654efc1a7da3af74a
Mahesh Sabnis <sabnis_m@hotmail.com>
2021-03-23 15:46:21 +0530

    Using Net Library

---
+++ NetFrwk_Library/ClsData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFrwk_Library
+{
+    public class ClsData
+    {
+        public List<Employee> GetEmployees()
+        {
+            return new Employees();
+        }
+        public int Add(int x,int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ NetFrwk_Library/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFrwk_Library
+{
+	public class Employee
+	{
+		public int EmpNo { get; set; }
+		public string EmpName { get; set; }
+	}
+
+	public class Employees : List<Employee>
+	{
+		public Employees()
+		{
+			Add(new Employee() { EmpNo = 101, EmpName = "A" });
+			Add(new Employee() { EmpNo = 102, EmpName = "B" });
+		}
+	}
+
+}
